689486b9092ed902b621c4d30550b49ef91d9a1d
SifatNiloy <sifatniloy18@gmail.com>
2023-07-29 15:07:36 +0600

    background image and text set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./pages/Shared/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">  
-      <h2>hello</h2>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="home">
+      <h2 className="text-4xl p-8">Welcome to doctors portal. How can we help?</h2>
+    </div>
+  );
+};
+
+export default Home;
