837c430839315c65d4fe3df402d21f03d657ed0c
SifatNiloy <sifatniloy18@gmail.com>
2023-07-29 15:43:43 +0600

    navbar primary setup done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,17 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home">
-      <h2 className="text-4xl p-8">Welcome to doctors portal. How can we help?</h2>
+      <h2 className="text-5xl p-8 welcome-text">
+        Welcome to doctors portal. How can we help?
+      </h2>
+      <div className="boxes">
+        <div className="box bg-green-500 text-xl text-white">
+          Find a location
+        </div>
+        <div className="box bg-blue-500 text-xl text-white">Find a doctor</div>
+        <div className="box bg-sky-500 text-xl text-white">Patient Portal</div>
+        <div className="box bg-cyan-500 text-xl text-white">Pay my bill</div>
+      </div>
     </div>
   );
 };
--- src/pages/Shared/Navbar/Navbar.js
+++ src/pages/Shared/Navbar/Navbar.js
@@ -1,8 +1,26 @@
 import React from "react";
-
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 const Navbar = () => {
+  const menuItems = (
+    <>
+      <li>
+        <Link to="">Home</Link>
+      </li>
+      <li>
+        <Link to="">Classes and events</Link>
+      </li>
+      <li>
+        <Link to="">Patients and visitors</Link>
+      </li>
+      <li>
+        <Link to="">Services and Specialities</Link>
+      </li>
+    </>
+  );
   return (
     <div className="navbar bg-base-100">
+      <div className="gradient-line"></div>
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -25,48 +43,14 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {menuItems}
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a className="btn btn-ghost normal-case text-2xl text-blue-800">Doctor Portal</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
+        <ul className="menu menu-horizontal px-1 text-lg">
+          {menuItems}
         </ul>
       </div>
       <div className="navbar-end">
