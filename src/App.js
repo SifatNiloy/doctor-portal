09b39ee8b06f8436e414dc2de75b01f2d1d1fea8
SifatNiloy <sifatniloy18@gmail.com>
2023-07-29 14:51:10 +0600

    default navbar added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 
+
 function App() {
   return (
     <div className="App">  
-    <h2>heyyyy</h2> 
-      <Routes>
-      
-      </Routes>
+      <h2>hello</h2>
     </div>
   );
 }
