352ed57d46c91e528affe96d4a735d6649525ca2
SifatNiloy <sifatniloy18@gmail.com>
2023-07-31 13:44:14 +0600

    firebase initialized and AuthProvider set done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import { router } from "./Routes/Routes";
 import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./contexts/AuthProvider/AuthProvider";
 
 function App() {
   return (
     <div className="App">
       <div className="max-w-[1440px] mx-auto text-accent">
-        <RouterProvider router={router} />     
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
       </div>
     </div>
   );
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,13 +1,72 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import initializeFirebase from "../Firebase/firebase.init";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
 
-initializeFirebase
-const useFirebase =()=>{
-    const [user, setUser]= useState({})
+initializeFirebase();
+const useFirebase = () => {
+  const [user, setUser] = useState({});
 
-    return {
-        user,
-    }
-}
+  const auth = getAuth();
+  const registerUser = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  };
+
+  const loginUser = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+  //observe user presense
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser({});
+      }
+    });
+
+    return () => unsubscribe;
+  }, []);
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
+  return {
+    user,
+    registerUser,
+    loginUser,
+    logout,
+  };
+};
 
 export default useFirebase;
